c5e490bd8ad8d5f40c7f13e594e83e563cd7fc65
zr <fly_boyabc@hotmail.com>
2018-03-07 18:16:17 +0800

    选文件夹，读取图片

---
+++ IDNumber/CCardImg.cpp
@@ -0,0 +1,27 @@
+﻿#include "stdafx.h"
+#include "CCardImg.h"
+
+
+CCardImg::CCardImg(string strFullPath)
+{
+	if (strFullPath.length() > 0) {
+		m_strFullPath = strFullPath;
+	}
+}
+
+
+CCardImg::~CCardImg()
+{
+}
+
+
+// 读取身份证图像
+bool CCardImg::ReadCardImg()
+{ 
+	m_origImg = imread(m_strFullPath.c_str(), IMREAD_COLOR);
+	
+	if (NULL == m_origImg.data)
+		return false;
+
+	return true;
+}
---
+++ IDNumber/CCardImg.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+#include <opencv2/imgcodecs.hpp>
+
+using namespace cv;
+
+class CCardImg
+{
+public:
+	CCardImg(string strFullPath);
+	~CCardImg();
+
+private:
+	// 原始图像
+	Mat m_origImg;
+	// 文件路径
+	string m_strFullPath;
+
+
+public:
+	// 读取身份证图像
+	bool ReadCardImg();
+	
+};
+
