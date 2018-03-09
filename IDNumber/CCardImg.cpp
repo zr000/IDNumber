2530ed1796e3580c89dae4fa364719addd6a4be2
zr <fly_boyabc@hotmail.com>
2018-03-09 14:32:21 +0800

    源图片显示

--- IDNumber/CCardImg.cpp
+++ IDNumber/CCardImg.cpp
@@ -20,16 +20,70 @@ bool CCardImg::ReadCardImg()
 { 
 	m_origImg = imread(m_strFullPath.c_str(), IMREAD_COLOR);
 	
-	if (NULL == m_origImg.data)
+	if (m_origImg.empty())
 		return false;
+
+	m_imgSize.cx = m_origImg.cols;
+	m_imgSize.cy = m_origImg.rows;
+
 
 	return true;
 }
 
 
-// 设置绘图dc
-void CCardImg::SetPaintDC(CDC* paintDC)
+#ifdef _WIN32
+
+void CCardImg::MatToCImage(Mat &mat, CImage &cImage)
 {
-	if (NULL != paintDC)
-		m_paintDC = paintDC;
+	int width = mat.cols;
+	int height = mat.rows;
+	int channels = mat.channels();
+
+	cImage.Destroy();
+	cImage.Create(width,
+		height,
+		8 * channels);
+
+	uchar* ps;
+	uchar* pimg = (uchar*)cImage.GetBits();
+	int step = cImage.GetPitch();
+
+	for (int i = 0; i < height; ++i)
+	{
+		ps = (mat.ptr<uchar>(i));
+		for (int j = 0; j < width; ++j)
+		{
+			if (channels == 1) //gray    
+			{
+				*(pimg + i * step + j) = ps[j];
+			}
+			else if (channels == 3) //color    
+			{
+				for (int k = 0; k < 3; ++k)
+				{
+					*(pimg + i * step + j * 3 + k) = ps[j * 3 + k];
+				}
+			}
+		}
+	}
+
 }
+
+void CCardImg::DrawImg(cv::Mat& mat, CDC * dc, CRect rect)
+{
+	if (mat.empty() || mat.depth() != CV_8U)
+		return;
+
+	cv::Mat matdraw;
+
+	cv::resize(mat, matdraw, cv::Size(rect.Width(), rect.Height()));
+
+	CImage imgdraw;
+
+	MatToCImage(matdraw, imgdraw);
+
+	imgdraw.Draw(dc->GetSafeHdc(), rect);
+}
+#endif // _WIN32
+
+
--- IDNumber/CCardImg.h
+++ IDNumber/CCardImg.h
@@ -1,5 +1,6 @@
 ﻿#pragma once
 #include <opencv2/imgcodecs.hpp>
+#include <opencv2/imgproc.hpp>
 
 using namespace cv;
 
@@ -10,19 +11,23 @@ public:
 	~CCardImg();
 
 private:
-	CDC* m_paintDC;
-
+	
 	// 原始图像
-	Mat m_origImg;
+	
 	// 文件路径
 	string m_strFullPath;
 
+	CSize m_imgSize;
 
 public:
 	// 读取身份证图像
 	bool ReadCardImg();
-	
-	// 设置绘图dc
-	void SetPaintDC(CDC* paintDC);
+	Mat m_origImg;
+
+
+#ifdef _WIN32
+	void DrawImg(cv::Mat& mat, CDC* dc, CRect rect);
+	void CCardImg::MatToCImage(Mat &mat, CImage &cImage);
+#endif // _WIN32
 };
 
